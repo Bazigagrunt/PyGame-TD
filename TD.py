6f049a7dcf5517265b1468f03e6563f9ae49c16e
Bazingagrunt <theo.tvrdy@gmail.com>
2025-07-11 18:05:35 +0200

    basic animation/moving, next rectangles

--- TD.py
+++ TD.py
@@ -2,19 +2,26 @@ import pygame as pygame
 pygame.init()
 
 width, height = 2560, 1440
+screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN, pygame.RESIZABLE)
+
 clock = pygame.time.Clock()
 test_surface = pygame.Surface((200,400))
 test_surface.fill((100, 100, 100))
-sky = pygame.image.load("Images/20.png")
-player = pygame.image.load("Images/16.png")
+sky = pygame.image.load("Images/20.png").convert()
+player = pygame.image.load("Images/16.png").convert_alpha()
 font = pygame.font.Font("Fonts/SUBWT.ttf", 35)
 font2 = pygame.font.Font("Fonts/Minecraft.ttf", 35)
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 testtext = font.render("I am Kled! High Major Commodore of the First Legion Third Multiplication Double Admiral Artillery Vanguard Company! You will respect my authority!", False, "yellow")
 testtext2 = font2.render("I am Kled! High Major Commodore of the First Legion Third Multiplication Double Admiral Artillery Vanguard Company! You will respect my authority!", True, "red")
+enemy = pygame.image.load("images/2.png").convert()
+enemy_xposition = 1800
+
+
+
+
 
 pygame.display.set_caption("Display")
-pygame.display.set_icon(pygame.image.load("Images/test.png"))
+pygame.display.set_icon(pygame.image.load("Images/test.png").convert_alpha())
 
 running = True
 while running:
@@ -27,6 +34,9 @@ while running:
     screen.blit(player, (0, 430))
     screen.blit(testtext, (0, 100))
     screen.blit(testtext2, (0, 140))
+    enemy_xposition -= 4
+    if enemy_xposition < 0: enemy_xposition = 1800
+    screen.blit(enemy, (enemy_xposition, 800))
 
     pygame.display.update()
     clock.tick(60)
