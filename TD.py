ddf003fa2755d5abde33909e1a444cc1e97dad15
Bazingagrunt <theo.tvrdy@gmail.com>
2025-12-01 22:25:26 +0100

    Rectangles, clean up

--- TD.py
+++ TD.py
@@ -1,24 +1,26 @@
 import pygame as pygame
+import math
 pygame.init()
 
-width, height = 2560, 1440
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN, pygame.RESIZABLE)
+width, height = 2000, 1000
+screen = pygame.display.set_mode((width, height))#, pygame.FULLSCREEN, pygame.RESIZABLE)
 
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((200,400))
-test_surface.fill((100, 100, 100))
+
 sky = pygame.image.load("Images/20.png").convert()
-player = pygame.image.load("Images/16.png").convert_alpha()
+color = (0, 0, 0)
+
 font = pygame.font.Font("Fonts/SUBWT.ttf", 35)
 font2 = pygame.font.Font("Fonts/Minecraft.ttf", 35)
-testtext = font.render("I am Kled! High Major Commodore of the First Legion Third Multiplication Double Admiral Artillery Vanguard Company! You will respect my authority!", False, "yellow")
-testtext2 = font2.render("I am Kled! High Major Commodore of the First Legion Third Multiplication Double Admiral Artillery Vanguard Company! You will respect my authority!", True, "red")
-enemy = pygame.image.load("images/2.png").convert()
-enemy_xposition = 1800
 
+#testtext = font.render("I am Kled! High Major Commodore of the First Legion Third Multiplication Double Admiral Artillery Vanguard Company! You will respect my authority!", False, "yellow")
+#testtext2 = font2.render("I am Kled! High Major Commodore of the First Legion Third Multiplication Double Admiral Artillery Vanguard Company! You will respect my authority!", True, "red")
 
+player_surf = pygame.image.load("Images/16.png").convert_alpha()
+player_rect = player_surf.get_rect(bottomleft = (0, 1000))
 
-
+enemy_surf = pygame.image.load("images/2.png").convert()
+enemy_rect = enemy_surf.get_rect(midbottom = (1700, 1000))
 
 pygame.display.set_caption("Display")
 pygame.display.set_icon(pygame.image.load("Images/test.png").convert_alpha())
@@ -28,15 +30,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((255, 255, 255))
-    screen.blit(test_surface, (200,100))
-    screen.blit(sky, (400, 200))
-    screen.blit(player, (0, 430))
-    screen.blit(testtext, (0, 100))
-    screen.blit(testtext2, (0, 140))
-    enemy_xposition -= 4
-    if enemy_xposition < 0: enemy_xposition = 1800
-    screen.blit(enemy, (enemy_xposition, 800))
+
+    #screen.blit(sky, (400, 200))
+    #screen.blit(testtext, (0, 100))
+    #screen.blit(testtext2, (0, 140))
+    screen.fill(color)
+
+
+    enemy_rect.x -= 4
+    if enemy_rect.right <= 0: enemy_rect.left = 2000
+    screen.blit(player_surf, player_rect)
+    screen.blit(enemy_surf, enemy_rect)
+   
+    #player_rect.left += 1
 
     pygame.display.update()
     clock.tick(60)
